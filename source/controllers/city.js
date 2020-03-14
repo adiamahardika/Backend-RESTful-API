34b947fbad0b9b1e26c31e59aeec403f23847268
Muhammad Syahendra Anindyantoro <muhsyahendraa1722@gmail.com>
2020-03-14 11:26:20 +0700

    add read city

--- source/controllers/city.js
+++ source/controllers/city.js
@@ -1,4 +1,4 @@
-const accountModel = require("../models/city");
+const cityModel = require("../models/city");
 const funcHelpers = require("../helpers");
 
 module.exports = {
@@ -9,11 +9,20 @@ module.exports = {
                 name_city: request.body.name_city,
                 id_province: request.body.id_province
             }
-            const result = await accountModel.createCity(data)
+            const result = await cityModel.createCity(data)
             funcHelpers.response(response, 200, result)
         } catch (error) {
             console.log(error)
             funcHelpers.cumstomErrorResponse(response, 404, 'Create City Failed!')
+        }
+    },
+    readCity: async (request, response) => {
+        try {
+            const result = await cityModel.readCity()
+            funcHelpers.response(response, 200, result)
+        } catch (error) {
+            console.log(error)
+            funcHelpers.customErrorResponse(response, 404, 'Create City Failed!')
         }
     }
 }
