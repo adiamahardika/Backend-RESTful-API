9d45bebf4cff5355baaaaf1d9b10bec70d17dd56
mannisar <42530153+mannisar@users.noreply.github.com>
2020-03-14 11:10:16 +0700

    add create subcity

--- source/index.js
+++ source/index.js
@@ -5,12 +5,14 @@ const accountRouter = require('./routes/account')
 const productRouter = require('./routes/product')
 const categoryRouter = require('./routes/category')
 const purchaseRouter = require('./routes/purchase')
+const subcityRouter = require('./routes/sub_city')
 
 Route
     .use('/account/', accountRouter)
     .use('/product/', productRouter)
     .use('/category/', categoryRouter)
     .use('/purchase/', purchaseRouter)
+    .use('/subcity/', subcityRouter)
     .use('/upload', express.static('./upload'))
 
 module.exports = Route
