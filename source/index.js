2fbb8fd4b687c26e96b68c241f500c20f90d34b7
Muhammad Syahendra Anindyantoro <muhsyahendraa1722@gmail.com>
2020-03-14 10:49:24 +0700

    add create city

--- source/index.js
+++ source/index.js
@@ -5,12 +5,13 @@ const accountRouter = require('./routes/account')
 const productRouter = require('./routes/product')
 const categoryRouter = require('./routes/category')
 const purchaseRouter = require('./routes/purchase')
-
+const cityRouter = require('./routes/city')
 Route
     .use('/account/', accountRouter)
     .use('/product/', productRouter)
     .use('/category/', categoryRouter)
     .use('/purchase/', purchaseRouter)
     .use('/upload', express.static('./upload'))
+    .use('/city/', cityRouter)
 
 module.exports = Route
